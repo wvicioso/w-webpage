f825e35bfe20f00e2a9b03173358665708b7b6eb
Wellington Vicioso <wellington.vicioso@gmail.com>
2016-11-25 03:28:28 -0500

    add style

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,11 +1,13 @@
-def home
-  render :home
-end
+class IndexController < ApplicationController
+  def home
+    render :home
+  end
 
-def sales
-  render :sales
-end
+  def sales
+    render :sales
+  end
 
-def contact
-  render :contact
+  def contact
+    render :contact
+  end
 end
